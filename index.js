09e9db3a9799a6d4196f3f4af935f7c200a5b129
Soila Torome <soila.torome@student.moringaschool.com>
2022-08-21 19:11:37 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someValue){
+  let result
+  if (someValue > 2500) {
+    result = 'No can do.';
+  }
+  else if(someValue > 2000) {
+    result = 'I will gladly take your thirty bucks.';
+  }
+  else {
+    result = 'This one is on me!';
+  }
+  return result
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(someCity){
+let result
+if(someCity === 'NYC') {
+  result = 'Ok, sounds good.';
+}
+else {
+  result = 'No go.' 
+}
+return result
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(someTip){
+let result
+if(someTip === 'generous') {
+  result = 'Thank you so much.';
+}
+else if(someTip === 'not as generous') {
+  result = 'Thank you.';
+}
+else {
+  result = 'Bye.';
+}
+return result
 }
